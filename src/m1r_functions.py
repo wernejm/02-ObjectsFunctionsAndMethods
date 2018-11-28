98cd1eb41e260e15b4b44336e1f75e3bb8502870
wernejm <wernejm@rose-hulman.edu>
2018-11-28 08:50:06 -0500

    Done.

--- src/m1r_functions.py
+++ src/m1r_functions.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO:  READ the code below. TRACE (by hand) the execution of the code,
+# DONE:  READ the code below. TRACE (by hand) the execution of the code,
 # predicting what will get printed.  Then run the code
 # and compare your prediction to what actually was printed.
 # Then mark this _TODO_ as DONE and commit-and-push your work.
@@ -31,3 +31,17 @@ def hello_and_goodbye(person1, person2):
 
 
 main()
+
+# I expect the code to print the following:
+# Hello, Snow White - how are things?
+# Goodbye, Bashful - see you later!
+#   Ciao!
+#   Bai bai!
+# Hello, Grumpy - how are things?
+# Hello, Sleepy - how are things?
+# Hello, Magic Mirror - how are things?
+# Goodbye, Cruel Queen - see you later!
+#   Ciao!
+#   Bai bai!
+# I initially didn't see the ciao/bai bai remarks below the initial "goodbye" print,
+# but I was able to properly trace everything.
